371173cc0ceab8753e366142a8ec578efb929952
Go Uehara <ueharanaha@gmail.com>
2021-08-25 11:56:41 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let i = 0;
+const speed = 400;
+const txt = "Hello world :-)";
+const video = document.getElementById("myVideo");
+
+const typeWriter = () => {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(() => {
+      video.pause();
+    }, 8000);
+  }
+};
+
+typeWriter();
