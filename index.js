9f1abc9ff5761bfcfc7fe70012f61326632f3992
gouehara <39700324+gouehara@users.noreply.github.com>
2021-09-08 12:20:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,26 +16,3 @@ const typeWriter = () => {
 };
 
 typeWriter();
-
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?q=Naha&appid=1fb11a8e3963b0a76a4000e12209fecc&lang=ja&units=metric";
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    const iconCode = data.weather[0].icon;
-    const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
-    const txt2 = `那覇市の天気は`;
-    let j = 0;
-
-    const typeWriter2 = () => {
-      if (j < txt2.length) {
-        document.getElementById("demo2").textContent += txt2.charAt(j);
-        j++;
-        setTimeout(typeWriter2, speed);
-      }
-    };
-    setTimeout(typeWriter2, 7200);
-    setTimeout(() => {
-      document.getElementById("wicon").src = iconUrl;
-    }, 10000);
-  });
